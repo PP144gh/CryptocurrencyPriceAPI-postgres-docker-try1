7e2e98eb94d7c0876f42754b53bd9627d39d03e9
pp144 <pedromanpereira@gmail.com>
2024-08-05 17:36:58 +0100

    dockerized application

--- backend/database/dbConfig.js
+++ backend/database/dbConfig.js
@@ -1,11 +1,16 @@
 const { Pool } = require('pg');
 
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'eventLogger',
+//   password: 'test',
+//   port: 5432,
+// });
+
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'eventLogger',
-  password: 'test',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL
 });
+
 
 module.exports = pool;
